65113d768abf7d1fbbb33828b0b33dea05bde680
Subhamay-Dey <ncdey1966@gmail.com>
2024-05-31 04:18:56 +0530

    Successsfully build the popup component when the scan finger will be oressed and it will also close with another button -- "Added react-icons, react-router-dom, and updated App.jsx to use react-router-dom. Modified popup.jsx to include an image and buttons."

---
+++ client/src/components/Home.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import Popup from './popup';
+import { useState } from 'react';
+import { CgProfile } from "react-icons/cg";
+import img1 from "../images/profile-logo-removebg-preview.png"
+import img2 from "../images/thumb_img.jpg"
+
+function Home() {
+
+    const [buttonPopup, setPopup] = useState(false);
+
+  return (
+    <div className='flex w-full h-screen justify-center items-center gap-[2px] relative'>
+    <div className='w-80 h-[450px] border-[2px] border-sky-400 bg-white'>
+        <img src={img1} alt="" size={10}/>
+        <button className='w-full h-[40px]  bg-sky-400 text-white'>Take Photo</button>
+        <button className='w-full h-[40px] mt-6 bg-sky-400 text-white'>No File chosen</button>
+    </div>
+    <div className='absolute top-18 left-18'>
+        <Popup trigger={buttonPopup} setTrigger={setPopup}>
+            
+        </Popup>
+    </div>
+    <div className='w-96 h-[450px] border-[2px] border-sky-400 bg-white'>
+        <img src={img2} alt="" className='w-[200px] h-[300px] flex justify-center items-center ml-20'/>
+        <button className='w-full h-[40px] mt-4 bg-sky-400 text-white' onClick={() => setPopup(true)}>Scan Finger</button>
+        <button className='w-full h-[40px] mt-6 bg-sky-400 text-white'>No File chosen</button>
+    </div>
+    </div>
+    
+  )
+}
+
+export default Home
--- client/src/components/popup.jsx
+++ client/src/components/popup.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import React from 'react';
+import img3 from "../images/biometric_img.jpg"
 
-function popup() {
-  return (
-    <div>popup</div>
-  )
+function Popup(props) {
+  return (props.trigger) ? (
+    <div className='w-[400px] h-[400px] bg-white border-[2px] border-sky-600 flex justify-evenly relative'>
+        <img src={img3} alt="" />
+    <div className='absolute top-80 flex justify-evenly gap-6'>
+        <button className='px-5 py-2 rounded-lg border-black border-[2px] bg-white'>Scan</button>
+        <button className='px-5 py-2 bg-purple-500 text-white rounded-md' onClick={() => props.setTrigger(false)}>Cancel</button>
+        {props.children}
+        <button className='px-5 py-2 rounded-lg border-black border-[2px] bg-white'>Verify</button>
+    </div>
+    </div>
+  ) : "";
 }
 
-export default popup
+export default Popup
